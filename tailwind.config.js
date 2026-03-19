6787407b8ff6104d5b73bc08e37c20cc9a7c225f
DevDinamics <armando.brn@outlook.com>
2026-03-18 21:07:31 -0600

    update config files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,ts,scss,css}", // 👈 Agregamos scss y css por si acaso
+    "./src/**/*.{html,ts,scss,css}",
     "./src/index.html",
   ],
   theme: {
