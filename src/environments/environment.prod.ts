a761acdd5a01ba26a479b5ae05eeac6b1d5193c6
DevDinamics <armando.brn@outlook.com>
2026-05-11 20:43:58 -0600

    primera versión de Blaze

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,10 +2,10 @@ export const environment = {
   production: true,
   firebase: {
     apiKey: "AIzaSyDAlmqkk74Oobm1VMUEp5ZCu9kQ9M2XaH0",
-  authDomain: "fit-app-e1a47.firebaseapp.com",
-  projectId: "fit-app-e1a47",
-  storageBucket: "fit-app-e1a47.firebasestorage.app",
-  messagingSenderId: "646180634299",
-  appId: "1:646180634299:web:9a86926169accaaded339e"
+    authDomain: "blaze3.netlify.app",
+    projectId: "fit-app-e1a47",
+    storageBucket: "fit-app-e1a47.firebasestorage.app",
+    messagingSenderId: "646180634299",
+    appId: "1:646180634299:web:9a86926169accaaded339e"
   }
 };
